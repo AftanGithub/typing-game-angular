758866614cd83815939e5f8b9fb5dfca781834cb
AFTAN <aftanahmadskills@gmail.com>
2023-04-13 20:15:43 +0530

    added_core_functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { lorem } from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+ 
+  randomText = lorem.sentence();
+  enteredText = "";
+
+  handleUserInput(e:Event){
+    const parsedData = e.target as HTMLInputElement;
+    console.log(parsedData.value);
+    this.enteredText = parsedData.value
+  
+  }
+
+  compare(randomLetter:string,enteredLetter:string){
+    if(!enteredLetter){
+      return 'pending'
+    }
+
+    return randomLetter === enteredLetter ? 'green':'red';
+  }
+
 }
